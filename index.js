f268d3cf312712945e747386ad22ae2ed876750a
arielEliasPerez <ariel65_ep@hotmail.com>
2024-02-14 23:33:40 -0300

    Puzzle v1

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 
-const nivel = 1;
+let nivel = 0;
 let posiciones = [];
 let piezasAcertadas = 0;
 let maxPiezasAcertadas = 6;
+let draggingCell = null;
 
 function crearPuzzle(){
-    console.log("Probando que ande");
+    nivel++;
+    posiciones = [];
+    piezasAcertadas = 0;
+    maxPiezasAcertadas = nivel*nivel*6;
+
     const containerGrid = document.getElementById("grid-container");
+    containerGrid.innerHTML = '';
 
-    containerGrid.style.gridTemplate ="repeat(3, 1fr) / repeat(2, 1fr)";
-
+    containerGrid.style.gridTemplate =`repeat(${3*nivel}, 1fr) / repeat(${2*nivel}, 1fr)`;
+    //getComputedStyle(document.querySelector('.pieza')).backgroundSize = `${-posX*100}% ${-posY*100}%`;
     for(let i = 0; i<nivel*3; i++){
         for(let j = 0; j<nivel*2; j++){
-
-            const pieza =document.createElement('div');
+            
+            const pieza = document.createElement('div');
             pieza.id = `p${i}${j}`;
             pieza.className = 'pieza';
+            cambiarBackground(pieza);
+            pieza.style.backgroundSize = `${nivel*200}% ${nivel*300}%`;
+            //pieza.setAttribute('style', `backgroun-size: ${nivel*200}% ${nivel*300}%`); 
+            
 
             const pos = generarPosicionRandom();
             const posY = Math.floor(pos/10);
             const posX = pos%10;
             pieza.style.backgroundPosition = `${-posX*100}% ${-posY*100}%`;
+            
 
             pieza.setAttribute('draggable', 'true');
             pieza.setAttribute('ondragstart', 'startDrag(event)');
@@ -28,26 +39,48 @@ function crearPuzzle(){
             pieza.setAttribute('ondragover', 'allowDrop(event)');
 
             containerGrid.appendChild(pieza);
-
+            
             verificarPosicionCorrecta(pieza, ()=>piezasAcertadas++);
         }
     }
 }
 
+function cambiarBackground(pieza){
+    switch(nivel){
+        case 1:
+            pieza.style.backgroundImage = `url('images/nivel1.jpg')`;
+            break;
+        case 2:
+            console.log('adentro');
+            pieza.style.backgroundImage = `url('images/nivel2.jpg')`;
+            break;
+        case 3:
+            pieza.style.backgroundImage = "url('images/nivel3.jpg')";
+            break;
+        case 4:
+            pieza.style.backgroundImage = "url('images/nivel4.jpg')";
+            break;
+        default:
+            pieza.style.backgroundImage = "url('images/nivel5.jpg')";
+    }
+    
+
+}
+
 function generarPosicionRandom(){    
-    let pos = Math.floor(Math.random()*2)  + 10 * Math.floor(Math.random()*3);
+    let pos = Math.floor(Math.random()*2*nivel)  + 10 * Math.floor(Math.random()*3*nivel);
 
     let i = 0;
     while(i < posiciones.length ){
         if(posiciones[i] === pos){
-            pos = Math.floor(Math.random()*2)  + 10 * Math.floor(Math.random()*3);
+            pos = Math.floor(Math.random()*2*nivel)  + 10 * Math.floor(Math.random()*3*nivel);
             i = 0;
         }
         else {
             i++;
         }
     }
-
+    
     posiciones.push(pos);
 
     return pos;
@@ -73,12 +106,15 @@ function drop(event) {
 
         const tempBackground = draggingCell.style.backgroundImage;
         const tempPosition = getComputedStyle(draggingCell).backgroundPosition;
-
+        
         draggingCell.style.background = targetCell.style.backgroundImage;
         draggingCell.style.backgroundPosition = getComputedStyle(targetCell).backgroundPosition;
 
         targetCell.style.backgroundImage = tempBackground;
         targetCell.style.backgroundPosition = tempPosition;
+
+        draggingCell.style.backgroundSize = `${nivel*200}% ${nivel*300}%`;
+        targetCell.style.backgroundSize = `${nivel*200}% ${nivel*300}%`;
 
         if(!sacadoIncorrecto)
             verificarIntercambio(draggingCell, targetCell);
@@ -125,7 +161,10 @@ function verificarIntercambio(celdaArrastrada, celdaObjetivo){
     const accionPosicionCorrecta = () =>{
         piezasAcertadas ++;
         if(piezasAcertadas === maxPiezasAcertadas)
-            console.log("PUZZLE COMPLETADO");
+            {
+                console.log("PUZZLE COMPLETADO");
+                crearPuzzle()
+            }
     };
 
     verificarPosicionCorrecta(celdaObjetivo, accionPosicionCorrecta);
